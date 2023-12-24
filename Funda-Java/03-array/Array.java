82bf7de9f1a2f789ad844df318fb09754a5bb384
anakeanes <anakeanes.h@gmail.com>
2023-12-25 00:52:04 +0700

    Find more about array basic command

--- Funda-Java/03-array/Array.java
+++ Funda-Java/03-array/Array.java
@@ -1,5 +1,20 @@
 public class Array {
     public static void main(String[] args) {
-        String[] name = {"Laumcing"}
+        String[] name = {"Laumcing", "Coffee", "Podpo"};
+        System.out.println(name[0]);
+        System.out.println(name[1]);
+        System.out.println(name[2]);
+
+        //add the value inside of the array
+        name[0] = "Java";
+        System.out.println(name[0]);
+
+        int[] numbers = {1,2,3,4,5};
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+
+        //to see the amount inside of the array
+        System.out.println("length: " + numbers.length);
     }
 }
---
+++ Funda-Java/04-loop/Loop.java
@@ -0,0 +1,6 @@
+public class Loop {
+    //the following is function, but in Java, we call it method
+    public static void main(String[] args) {
+        
+    }
+}
