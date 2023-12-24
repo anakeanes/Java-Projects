4c963a3548a1d74c976a48f8e1bb936ecb4395b9
anakeanes <anakeanes.h@gmail.com>
2023-12-25 00:25:09 +0700

    example for the type of variables (Primitives Data) in Java

--- Funda-Java/01-starting-point/Hello.java
+++ Funda-Java/01-starting-point/Hello.java
@@ -5,9 +5,29 @@
 //File name and Class name must be the same, or it will failed to comply
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    //console.log("Hello World") in JavaScript
-    //fmt.Println("Hello World") in Go
-    //print("Hello World") in Python
+        //Java is a statically language, it need to be known the type at the compiling time
+        // <type> <name> = <value>
+        String msg = "Hello World!";
+        System.out.println(msg);
+
+        //console.log("Hello World") in JavaScript
+        //fmt.Println("Hello World") in Go
+        //print("Hello World") in Python
+
+        int loanTerm = 36;
+        System.out.println(loanTerm);
+
+        float interestRate = 7.5f; //for float we have to end with f, float with 7 digits, double go with 15 digits (standard)
+        System.out.println(interestRate); 
+
+
+        double loanAmount = 10000.0d;
+        System.out.println(loanAmount);
+
+        boolean isApproved = true;
+        System.out.println(isApproved);
+
+        char grade = 'A';
+        System.out.println(grade);
     }
 }
