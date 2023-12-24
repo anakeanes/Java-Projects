263cf01636f00c678e685d8bfa2c3865438a3218
anakeanes <anakeanes.h@gmail.com>
2023-12-25 01:15:50 +0700

    Conditioning Loop (if, else if, else)

--- Funda-Java/04-loop/Loop.java
+++ Funda-Java/04-loop/Loop.java
@@ -1,21 +1,18 @@
 public class Loop {
     //the following is function, but in Java, we call it method
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i ++) {
-            System.out.println(i);
-        }
+        
         System.out.println(); // for new line
         int[] numbers = {1,2,3,4,5};
         for (int i =0 ; i < numbers.length; i ++) {
-            System.out.println(numbers[i]);
+            int n = numbers[i];
+            if (n % 2 == 0 ) {
+                System.out.println(n + " is even.");
+            }else if ( n == 3) {
+                System.out.println(n + " is three.");
+            }else {
+                System.out.println(n + " is odd.");
+            }
         }
-        System.out.println(); // for new line
-        int i = 0;
-        while (i < 5){
-            System.out.println(i);
-            i ++;
-        }
-
-        
     }
 }
