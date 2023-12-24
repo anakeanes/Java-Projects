2d8e37a11b91ebafdc3c5e7d8acc89ca1fd1da3e
anakeanes <anakeanes.h@gmail.com>
2023-12-25 01:52:58 +0700

    Example of the method casting class, but yet still a fix value in the subclass

--- Funda-Java/06-class/Loan.java
+++ Funda-Java/06-class/Loan.java
@@ -1,5 +1,15 @@
+//"Class should be a template, structure, or a bluprint of the object"
+
 public class Loan {
-    public static void main(String[] args) {
-        
+    // don't have to define public static void main
+        String applicantName = "Laumcing";
+        double loanAmount = 10000.0d;
+        float interestRate = 7.5f;
+    
+    //if we don't want the output to be hashed, we use method "to String"
+    //to notice method, we use modifier, "public", "private", "protected" for example
+    public String toString() {
+        return "Applicant Name: " + applicantName + "\nLoan Amount: " + loanAmount +"\nInterest Rate: " + interestRate;
     }
 }
+
--- Funda-Java/06-class/Main.java
+++ Funda-Java/06-class/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String applicantName = "Laumcing";
+    /*     String applicantName = "Laumcing";
         double loanAmount = 10000.0d;
         float interestRate = 7.5f;
         
@@ -8,7 +8,7 @@ public class Main {
         System.out.println("Applicant Name: " + applicantName);
         System.out.println("Loan Amount: " + loanAmount);
         System.out.println("Interest Rate: " + interestRate);
-
+    */
         //Three of these output should group into one "object"
         /*JavaScript Object Notation (JSON)
          * var loan = {
@@ -26,6 +26,18 @@ public class Main {
          * InterestRate float32
          * }
          */
+
+        //call function to use, use command "new"
+        //"new" is use to class the instance
+        Loan john = new Loan();
+        System.out.println(john); // the output will comeout "Loan@hashed_object"
+        //we don't have to comply the class "Loan", if you comply main, it will comply to the related class automatically
+        
+        System.out.println();
+        
+        Loan laumcing = new Loan();
+        System.out.println(laumcing);
+
     }
 
 }
