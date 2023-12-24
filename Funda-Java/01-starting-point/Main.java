4bc46e9fdad80b58b586cf9756eb56f99d0e1c5d
anakeanes <anakeanes.h@gmail.com>
2023-12-25 00:06:47 +0700

    Try print out via standard output with "Hello World!"

--- Funda-Java/01-starting-point/Main.java
+++ Funda-Java/01-starting-point/Main.java
@@ -1,7 +1,13 @@
-//command+shift+p, as shortcut
 
+//command+shift+p, as shortcut
+//javac for complier --> will get the .class file (byte code)
+//java for run (if we run, it will run byte code file, not the java file)
+//File name and Class name must be the same, or it will failed to comply
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    //console.log("Hello World") in JavaScript
+    //fmt.Println("Hello World") in Go
+    //print("Hello World") in Python
     }
 }
